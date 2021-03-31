c84238b1ed45746dc335a9ca915fa030124c467a
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-03-31 14:21:25 +0200

    Initial Commit

---
+++ HWS-05-WordScramble/HWS_05_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_05_WordScrambleApp.swift
+//  HWS-05-WordScramble
+//
+//  Created by Manuel Casocavallo on 31/03/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_05_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
